ab1330714c7df5daad7fb36187ecb7888afae10f
Omoniyi Ilesanmi <omoniyi.ilesanmi@prunny.com>
2022-06-16 16:12:06 +0100

    fix setup compilation error

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,17 @@
-// const express = require("express")
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Welcome to LNStackTips Server');
+});
+
+app.listen(port, () => {
+    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
