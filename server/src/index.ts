71357f1a337dbd38c0bc2c59d5ae8c4540078660
Omoniyi Ilesanmi <omoniyi.ilesanmi@prunny.com>
2022-06-16 19:58:14 +0100

    create answer route

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import questionService from "./question-service";
+import answerService from "./answer-service";
 import bodyParser from "body-parser";
 
 
@@ -18,6 +19,8 @@ app.use(bodyParser.json());
 app.get('/', (req: Request, res: Response) => {
     res.send('Welcome to LNStackTips Server');
 });
+
+//QUESTION ROUTEs
 
 app.get(apiBase + '/questions', async (req, res, next) => {
     try {
@@ -40,17 +43,54 @@ app.post(apiBase + '/question', async (req, res, next) => {
     try {
         const {questionTitle, questionBody, tags, isRewardable, rewardInSatoshi, voteThreshold} = req.body;
         if (!questionTitle || !questionBody || !isRewardable) {
-            throw new Error('Any of Fields questionTitle, questionBody, isRewardable are required to make a question');
+            res.status(400).json({ error: 'Any of Fields questionTitle, questionBody, isRewardable are required to make a question'});
         }
         const isRewardableBoolean = Boolean(isRewardable);
         let numberRewardInSatoshi = parseInt(rewardInSatoshi);
         let numberVoteThreshold = parseInt(voteThreshold);
         if (isRewardable) {
             if (!rewardInSatoshi || !voteThreshold) {
-                throw new Error('Fields rewardInSatoshi and voteThreshold are required if Rewardable is true');
+                res.status(400).json({ error: 'Fields rewardInSatoshi and voteThreshold are required if Rewardable is true'});
             }
         }
         questionService.createQuestion(questionTitle, questionBody, tags, isRewardableBoolean, numberRewardInSatoshi, numberVoteThreshold, '')
+        res.sendStatus(201);
+    } catch(err) {
+        next(err);
+    }
+});
+
+
+//ANSWER ROUTES
+app.get(apiBase + '/answers', async (req, res, next) => {
+    try {
+        res.json({ data: answerService.getAllAnswers() });
+    } catch(err) {
+        next(err);
+    }
+});
+
+app.get(apiBase + '/answer/:id', (req, res) => {
+    const answerById = answerService.getAnswerById(parseInt(req.params.id));
+    if (answerById) {
+        res.json({ data: answerById });
+    } else {
+        res.status(404).json({ error: `No Question found with ID ${req.params.id}`});
+    }
+});
+
+app.post(apiBase + '/answer', async (req, res, next) => {
+    try {
+        const {questionId, answerBody, claimHash} = req.body;
+        if (!questionId || !answerBody) {
+            res.status(400).json({ error: 'Any of Fields questionId and answerBody are required to make a answer'});
+        }
+        const numberQuestionId = parseInt(questionId);
+        const question = questionService.getQuestionById(numberQuestionId);
+        if(!question){
+            res.status(400).json({ error: `Invalid Question  ID ${questionId}`});
+        }
+        answerService.createAnswer(numberQuestionId, answerBody, claimHash)
         res.sendStatus(201);
     } catch(err) {
         next(err);
